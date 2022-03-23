4528f6377af9a6eb56b60c3e10d4963d23d70e93
Tiedy7 <92095486+Tiedy7@users.noreply.github.com>
2022-03-23 08:44:53 -0400

    Add files via upload

---
+++ Schreckliche Seuche/src/core/Simulation.java
@@ -0,0 +1,65 @@
+package core;
+
+import java.util.ArrayList;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+
+public class Simulation extends BasicGameState 
+
+{
+	int id;
+	
+	
+	Simulation(int id) 
+	{
+		this.id = id;
+	}
+	
+	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException 
+	{
+		// This code happens when you enter a game state for the *first time.*
+		gc.setShowFPS(true);
+		
+		
+	}
+
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException 
+	{
+		
+		
+	}
+	
+
+
+	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException
+	{	
+		
+	}
+
+	public void enter(GameContainer gc, StateBasedGame sbg) throws SlickException 
+	{
+		
+	}
+
+	public void leave(GameContainer gc, StateBasedGame sbg) 
+	{
+		
+	}
+	
+	
+
+	// Returns the ID code for this game state
+	public int getID() 
+	{
+		return id;
+	}
+}
