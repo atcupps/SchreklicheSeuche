65ba26edaa8b7c27f7183edb18d88714b13a77dd
Tiedy7 <92095486+Tiedy7@users.noreply.github.com>
2022-05-03 18:49:11 -0400

    Update Settings.java

--- Schreckliche Seuche/src/core/Settings.java
+++ Schreckliche Seuche/src/core/Settings.java
@@ -21,12 +21,23 @@ public class Settings extends BasicGameState
 {
 	int id;
 	
-
+	Image background = null;
 	
 	Image hello = null;
 	
+	Image sliderBack = null;
+	Image sliderClick = null;
+	
 	Button button;
 	
+	Slider popDensity;
+	Slider rNaught;
+	
+	public static float rNaughtF;
+	public static int popDensityF;
+	
+	
+	public ArrayList<Slider> sliders;
 	
 	public ArrayList<Button> buttons;
 	
@@ -34,7 +45,7 @@ public class Settings extends BasicGameState
 	{
 		this.id = id;
 		buttons = new ArrayList<Button>();
-		
+		sliders = new ArrayList<Slider>();
 		
 		
 	}
@@ -44,28 +55,54 @@ public class Settings extends BasicGameState
 		// This code happens when you enter a game state for the *first time.*
 		gc.setShowFPS(true);
 		
-		hello = new Image("res/launch.png");
+		hello = new Image("res/launchButton.png");
 		hello.setFilter(Image.FILTER_NEAREST);
-			
-		button = new Button(400f,200f,400f,200f, hello);
+		
+		background = new Image("res/settingBackgroundNEW.png");
+		background.setFilter(Image.FILTER_NEAREST);
+		
+		button = new Button(50f,50f,200f,100f, hello);
+		
+		
+		sliderBack = new Image("res/slider.png");
+		sliderBack.setFilter(Image.FILTER_NEAREST);
+		
+		sliderClick = new Image("res/launchButton.png");
+		sliderClick.setFilter(Image.FILTER_NEAREST);
+		
+		popDensity = new Slider(150f,900f,400f,15f, sliderClick, sliderBack, 6);
+		
+		rNaught = new Slider(1150f,800f,700f,15f, sliderClick, sliderBack, 53);
 		
 		buttons.add(button);
-	
+		sliders.add(popDensity);
+		sliders.add(rNaught);
+		
+		popDensityF = 1;
+		rNaughtF = 0;
+		
 	}
 
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException 
 	{
+		background.draw(0,0,gc.getWidth(),gc.getHeight());
+		
 		for(Button b : buttons) {
 			b.render(gc, g);
 		}
-		
+		for(Slider b : sliders) {
+			b.render(gc, g);
+		}
 		
 		g.setColor(new Color(200,200,200));
 		
 		if (button.click()) {
-			g.drawRect(300, 300, 400, 400);
+			g.drawRect(300, 300, 100, 100);
 			sbg.enterState(2);
 		}
+		g.setColor(new Color(0,0,0));
+		g.drawString("" + popDensityF, 175, 950);
+		g.drawString("" + rNaughtF, 1175, 950);
 		
 	}
 	
@@ -76,6 +113,13 @@ public class Settings extends BasicGameState
 		for(Button b : buttons) {
 			b.update(gc);
 		}
+		for(Slider b : sliders) {
+			b.update(gc);
+		}
+		
+		popDensityF = (int) popDensity.categorySelected();
+		rNaughtF = rNaught.categorySelected()/5;
+		System.out.println(rNaughtF);
 	}
 
 	public void enter(GameContainer gc, StateBasedGame sbg) throws SlickException 
